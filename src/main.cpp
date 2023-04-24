3cec26b0123603435cc7fa151a62645315f55212
NatnaelTaddese <natnaeltaddese3@gmail.com>
2023-04-24 09:44:49 -0700

    initial header text set

--- src/main.cpp
+++ src/main.cpp
@@ -1,87 +1,37 @@
 /*******************************************************************************************
  *
- *   raylib [core] example - Basic window
- *
- *   Welcome to raylib!
- *
- *   To test examples, just press F6 and execute raylib_compile_execute script
- *   Note that compiled executable is placed in the same folder as .c file
- *
- *   You can find all basic examples on C:\raylib\raylib\examples folder or
- *   raylib official webpage: www.raylib.com
- *
- *   Enjoy using raylib. :)
- *
- *   Example originally created with raylib 1.0, last time updated with raylib 1.0
- *
- *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
- *   BSD-like license that allows static linking with closed source software
- *
+ *   raylib [core] example - Basic windows
  *   Copyright (c) 2013-2023 Ramon Santamaria (@raysan5)
  *
  ********************************************************************************************/
 
 #include "raylib.h"
 
-#define SNAKE_LENGTH 256
-#define SQUARE_SIZE 31
-static Vector2 offset = {0};
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
-
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 700;
+    const int screenHeight = 600;
+    const int screenOffset = 50;
 
-    offset.x = screenWidth % SQUARE_SIZE;
-    offset.y = screenHeight % SQUARE_SIZE;
+    InitWindow(screenWidth, screenHeight, "2048");
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetTargetFPS(60);
 
-    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        {
-            // Draw grid lines
-            for (int i = 0; i < screenWidth / SQUARE_SIZE + 1; i++)
-            {
-                DrawLineV((Vector2){SQUARE_SIZE * i + offset.x / 2, offset.y / 2}, (Vector2){SQUARE_SIZE * i + offset.x / 2, screenHeight - offset.y / 2}, LIGHTGRAY);
-            }
-
-            for (int i = 0; i < screenHeight / SQUARE_SIZE + 1; i++)
-            {
-                DrawLineV((Vector2){offset.x / 2, SQUARE_SIZE * i + offset.y / 2}, (Vector2){screenWidth - offset.x / 2, SQUARE_SIZE * i + offset.y / 2}, LIGHTGRAY);
-            }
-        }
-
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        EndDrawing();
+        // header text defaults
+        DrawText("2048", screenOffset, screenOffset / 2, 70, LIGHTGRAY);
+        DrawText("HighestScore", screenWidth / 2, screenOffset / 2, 20, LIGHTGRAY);
+        DrawText("CurrentScore", screenWidth / 2 + 150, screenOffset / 2, 20, LIGHTGRAY);
 
-        //----------------------------------------------------------------------------------
+        EndDrawing();
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow(); // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
 
     return 0;
 }
