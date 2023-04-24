8a500f76a72bca43c4ef7848fb3698d685d38cdb
NatnaelTaddese <natnaeltaddese3@gmail.com>
2023-04-24 13:41:59 -0700

    defined global variables

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,22 @@
 #include <string>
 #include <iostream>
 
+typedef struct tile
+{
+    Vector2 position;
+    Color color;
+
+} squareTile;
+
+// --------------------
+// GLOBAL VARIABLES
+const int screenWidth = 600;
+const int screenHeight = 750;
+const int screenOffset = 50;
+const int squareSize = 120;
+const int lineWidth = 10;
+const int tileSize = squareSize - 10;
+
 void slideTilesLeft();
 void slideTilesRight();
 void slideTilesUp();
@@ -23,26 +39,26 @@ void drawBoard(int screenOffset, int squareSize)
     for (int i = 0; i < 5; i++)
     {
         int spacing = squareSize * i;
-        DrawRectangle(screenOffset, ((screenOffset / 2) + 150) + spacing, (squareSize * 4) + 10, 10, GRAY);
+        DrawRectangle(screenOffset, ((screenOffset / 2) + 150) + spacing, (squareSize * 4) + lineWidth, lineWidth, GRAY);
     }
     // vertical grid
     for (int i = 0; i < 5; i++)
     {
         int spacing = squareSize * i;
-        DrawRectangle(screenOffset + spacing, ((screenOffset / 2) + 150), 10, (squareSize * 4), GRAY);
+        DrawRectangle(screenOffset + spacing, ((screenOffset / 2) + 150), lineWidth, (squareSize * 4), GRAY);
     }
+}
+
+void initGame()
+{
+    InitWindow(screenWidth, screenHeight, "2048");
+
+    SetTargetFPS(60);
 }
 
 int main(void)
 {
-    const int screenWidth = 600;
-    const int screenHeight = 750;
-    const int screenOffset = 50;
-    const int squareSize = 120;
-
-    InitWindow(screenWidth, screenHeight, "2048");
-
-    SetTargetFPS(60);
+    initGame();
 
     while (!WindowShouldClose())
     {
@@ -51,19 +67,23 @@ int main(void)
         ClearBackground(RAYWHITE);
 
         // header text defaults
-        DrawText("2048", screenOffset, screenOffset / 2, 70, LIGHTGRAY);
+        DrawText("2048", screenOffset, screenOffset / 2, 80, LIGHTGRAY);
 
-        DrawRectangle((screenWidth / 2) - 10, (screenOffset / 2) - 10, 155, 100, DARKGRAY);
-        DrawText("HighestScore", screenWidth / 2, screenOffset / 2, 20, LIGHTGRAY);
-        DrawText("CurrentScore", screenWidth / 2 + 155, screenOffset / 2, 20, LIGHTGRAY);
+        DrawRectangle(screenOffset + lineWidth + 3 * squareSize, (screenOffset / 2) - lineWidth, tileSize, tileSize, BROWN);
+        DrawText("Score", screenOffset + (2 * lineWidth) + (3 * squareSize), screenOffset / 2, 20, LIGHTGRAY);
+
         // draw board
         drawBoard(screenOffset, squareSize);
+
+        // // tile
+        DrawRectangle(screenOffset + 10, ((screenOffset / 2) + 150) + lineWidth, tileSize, tileSize, LIGHTGRAY);
 
         if (IsKeyPressed(KEY_LEFT))
         {
         }
         if (IsKeyPressed(KEY_RIGHT))
         {
+            // DrawRectangle((screenOffset + 10) + squareSize, ((screenOffset / 2) + 150) + 10, tileSize, tileSize, RED);
         }
         if (IsKeyPressed(KEY_UP))
         {
