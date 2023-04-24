585834493f3b7515eb34c3f049e9f1a5793f07f6
NatnaelTaddese <natnaeltaddese3@gmail.com>
2023-04-24 11:13:39 -0700

    drew the game board and grid

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,32 @@
  ********************************************************************************************/
 
 #include "raylib.h"
+#include <string>
+#include <iostream>
+
+void slideTilesLeft();
+void slideTilesRight();
+void slideTilesUp();
+void slideTilesDown();
+
+void drawBoard(int screenOffset, int squareSize)
+{
+    // grid background rectangle
+    DrawRectangle(screenOffset, (screenOffset / 2) + 150, (squareSize * 4), (squareSize * 4), DARKGRAY);
+
+    // horizontal grid
+    for (int i = 0; i < 5; i++)
+    {
+        int spacing = squareSize * i;
+        DrawRectangle(screenOffset, ((screenOffset / 2) + 150) + spacing, (squareSize * 4) + 10, 10, GRAY);
+    }
+    // vertical grid
+    for (int i = 0; i < 5; i++)
+    {
+        int spacing = squareSize * i;
+        DrawRectangle(screenOffset + spacing, ((screenOffset / 2) + 150), 10, (squareSize * 4), GRAY);
+    }
+}
 
 int main(void)
 {
@@ -26,24 +52,24 @@ int main(void)
 
         // header text defaults
         DrawText("2048", screenOffset, screenOffset / 2, 70, LIGHTGRAY);
+
         DrawRectangle((screenWidth / 2) - 10, (screenOffset / 2) - 10, 155, 100, DARKGRAY);
         DrawText("HighestScore", screenWidth / 2, screenOffset / 2, 20, LIGHTGRAY);
         DrawText("CurrentScore", screenWidth / 2 + 155, screenOffset / 2, 20, LIGHTGRAY);
+        // draw board
+        drawBoard(screenOffset, squareSize);
 
-        // grid background rectangle
-        DrawRectangle(screenOffset, (screenOffset / 2) + 150, (squareSize * 4), (squareSize * 4), DARKGRAY);
-
-        // horizontal grid
-        for (int i = 0; i < 5; i++)
+        if (IsKeyPressed(KEY_LEFT))
         {
-            int spacing = squareSize * i;
-            DrawRectangle(screenOffset, ((screenOffset / 2) + 150) + spacing, (squareSize * 4) + 10, 10, GRAY);
         }
-        // vertical grid
-        for (int i = 0; i < 5; i++)
+        if (IsKeyPressed(KEY_RIGHT))
         {
-            int spacing = squareSize * i;
-            DrawRectangle(screenOffset + spacing, ((screenOffset / 2) + 150), 10, (squareSize * 4), GRAY);
+        }
+        if (IsKeyPressed(KEY_UP))
+        {
+        }
+        if (IsKeyPressed(KEY_DOWN))
+        {
         }
 
         EndDrawing();
